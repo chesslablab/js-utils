2abf25776beaa6a19ff014a6cd47cfcac8e55181
programarivm <info@programarivm.com>
2024-02-15 10:30:22 +0100

    Added example

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+import Movetext from './src/Movetext.js';
 import Nag from './src/Nag.js';
 import Pgn from './src/Pgn.js';
 
+console.log(Movetext.toRows('1.e4 e5 2.Nf3'));
 console.log(Nag.glyphs);
 console.log(Pgn.symbol);
