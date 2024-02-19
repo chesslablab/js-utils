871e72a2a62b8ad817382a815eb7f3f4635be4ec
programarivm <info@programarivm.com>
2024-02-19 16:06:01 +0100

    Updated HistoryButtons

--- src/HistoryButtons.js
+++ src/HistoryButtons.js
@@ -26,19 +26,23 @@ export class HistoryButtons {
     });
 
     this.el.children.item(1).addEventListener('click', () => {
-      this.settings.sanMovesTable.setCurrent(this.settings.sanMovesTable.getCurrent() - 1);
-      this.settings.sanMovesTable.getSettings().chessboard.setPosition(
-        this.settings.sanMovesTable.getSettings().fen[this.settings.sanMovesTable.getCurrent()], true
-      );
-      this.settings.sanMovesTable.render();
+      if (this.settings.sanMovesTable.getCurrent() > 0) {
+        this.settings.sanMovesTable.setCurrent(this.settings.sanMovesTable.getCurrent() - 1);
+        this.settings.sanMovesTable.getSettings().chessboard.setPosition(
+          this.settings.sanMovesTable.getSettings().fen[this.settings.sanMovesTable.getCurrent()], true
+        );
+        this.settings.sanMovesTable.render();
+      }
     });
 
     this.el.children.item(2).addEventListener('click', () => {
-      this.settings.sanMovesTable.setCurrent(this.settings.sanMovesTable.getCurrent() + 1);
-      this.settings.sanMovesTable.getSettings().chessboard.setPosition(
-        this.settings.sanMovesTable.getSettings().fen[this.settings.sanMovesTable.getCurrent()], true
-      );
-      this.settings.sanMovesTable.render();
+      if (this.settings.sanMovesTable.getCurrent() < this.settings.sanMovesTable.getSettings().fen.length - 1) {
+        this.settings.sanMovesTable.setCurrent(this.settings.sanMovesTable.getCurrent() + 1);
+        this.settings.sanMovesTable.getSettings().chessboard.setPosition(
+          this.settings.sanMovesTable.getSettings().fen[this.settings.sanMovesTable.getCurrent()], true
+        );
+        this.settings.sanMovesTable.render();
+      }
     });
 
     this.el.children.item(3).addEventListener('click', () => {
