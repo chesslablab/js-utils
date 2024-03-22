858855df1bf8a4e60b81f3c2ddfb297ef0c0e845
programarivm <info@programarivm.com>
2024-03-22 18:39:17 +0100

    Updated MovesMetadataTable

--- src/MovesMetadataTable.js
+++ src/MovesMetadataTable.js
@@ -1,34 +1,81 @@
 import AbstractComponent from '../src/AbstractComponent.js';
 
 export class MovesMetadataTable extends AbstractComponent {
+  _leftTd() {
+    const leftTd = document.createElement('td');
+
+    if (this.props?.White) {
+      leftTd.appendChild(document.createTextNode(`${this.props.White}`));
+      leftTd.appendChild(document.createElement('br'));
+    }
+
+    if (this.props?.WhiteElo) {
+      leftTd.appendChild(document.createTextNode(`${this.props.WhiteElo}`));
+    }
+
+    return leftTd;
+  }
+
+  _centerTd() {
+    const centerTd = document.createElement('td');
+
+    let data = '';
+
+    if (this.props?.Result) {
+      centerTd.appendChild(document.createTextNode(`${this.props.Result}`));
+      centerTd.appendChild(document.createElement('br'));
+    }
+
+    if (this.props?.Event) {
+      data += `${this.props.Event}, `;
+    }
+
+    if (this.props?.Round) {
+      data += `Rd ${this.props.Round}, `;
+    }
+
+    if (this.props?.Site) {
+      data += `${this.props.Site}, `;
+    }
+
+    if (this.props?.Date) {
+      data += `${this.props.Date}, `;
+    }
+
+    if (this.props?.ECO) {
+      data += `${this.props.ECO}, `;
+    }
+
+    centerTd.appendChild(document.createTextNode(data.slice(0, -2)));
+    centerTd.classList.add('text-center');
+
+    return centerTd;
+  }
+
+  _rightTd() {
+    const rightTd = document.createElement('td');
+
+    if (this.props?.Black) {
+      rightTd.appendChild(document.createTextNode(`${this.props.Black}`));
+      rightTd.appendChild(document.createElement('br'));
+    }
+
+    if (this.props?.BlackElo) {
+      rightTd.appendChild(document.createTextNode(`${this.props.BlackElo}`));
+    }
+
+    rightTd.classList.add('text-end');
+
+    return rightTd;
+  }
+
   mount() {
     this._el.replaceChildren();
 
     const tr = document.createElement('tr');
-    const leftTd = document.createElement('td');
-    const centerTd = document.createElement('td');
-    const rightTd = document.createElement('td');
-
-    centerTd.classList.add('text-center');
-    rightTd.classList.add('text-end');
-
-    leftTd.appendChild(document.createTextNode(`${this.props.White}`));
-    leftTd.appendChild(document.createElement('br'));
-    leftTd.appendChild(document.createTextNode(`${this.props.WhiteElo}`));
-
-    centerTd.appendChild(document.createTextNode(`${this.props.Result}`));
-    centerTd.appendChild(document.createElement('br'));
-    centerTd.appendChild(document.createTextNode(`${this.props.Event}`));
-    centerTd.appendChild(document.createElement('br'));
-    centerTd.appendChild(document.createTextNode(`${this.props.Site}, ${this.props.Date}, ${this.props.ECO}`));
-
-    rightTd.appendChild(document.createTextNode(`${this.props.Black}`));
-    rightTd.appendChild(document.createElement('br'));
-    rightTd.appendChild(document.createTextNode(`${this.props.BlackElo}`));
-
-    tr.appendChild(leftTd);
-    tr.appendChild(centerTd);
-    tr.appendChild(rightTd);
+    tr.appendChild(this._leftTd());
+    tr.appendChild(this._centerTd());
+    tr.appendChild(this._rightTd());
 
     this._el.appendChild(tr);
   }
